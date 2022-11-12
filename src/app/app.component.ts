a3341e3f6f5a37b34d963ad5aec77f788323a4c8
Kabelo Masemula <ksmasemula@gmail.com>
2022-11-12 23:11:04 +0200

    lazy and dirty implementation of  calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgCalculator';
+
+ public display: string = '';
+ public errMessageMode: boolean = false;
+ public errMsg: string = '';
+
+  public getOperand(operand: number): void {
+    this.display += operand.toString();
+  }
+
+  public getOperator(operator: string): void {
+    this.errMessageMode = false;
+
+    if (operator === 'C') {
+      this.display = '';
+      return;
+    }
+
+    if (operator === '=') {
+      try {
+        let result = eval(this.display);
+
+        if (isNaN(result) || result.toString() === 'Infinity') {
+          this.errMsg = 'Invalid operation occured';
+          this.errMessageMode = true;
+          return;
+        }
+
+        this.display = result.toString();
+        return;
+      } catch (error: any) {
+        this.errMsg = error.message;
+        this.errMessageMode = true;
+        return;
+      }
+    }
+
+    this.display += operator;
+  }
 }
